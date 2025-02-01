d905545049cb49f9691b180ec2d51438cae7d339
cortez <89983388+cortez24rus@users.noreply.github.com>
2025-02-01 23:58:11 +0700

    update

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ curl -L https://github.com/cortez24rus/motd/archive/main.tar.gz | tar -zxv
 mkdir -p /etc/update-motd.d/old-motd
 find /etc/update-motd.d/ -maxdepth 1 -type f -exec mv {} /etc/update-motd.d/old-motd/ \;
 
-mv main/motd/* /etc/update-motd.d/
+mv motd-main/motd/* /etc/update-motd.d/
 rm -rf motd-main
 
 if ! grep -q "session    optional     pam_motd.so motd=/run/motd.dynamic" /etc/pam.d/sshd; then
