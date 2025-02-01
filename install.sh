e804bba928eb35a4288368c30624a6e5396d1ca7
cortez <89983388+cortez24rus@users.noreply.github.com>
2025-02-02 04:57:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
+# Check OS and set release variable
+if [[ -f /etc/os-release ]]; then
+    source /etc/os-release
+    release=$ID
+elif [[ -f /usr/lib/os-release ]]; then
+    source /usr/lib/os-release
+    release=$ID
+else
+    echo "Failed to check the system OS, please contact the author!" >&2
+    exit 1
+fi
+echo "The OS release is: $release"
+
 apt update
 apt install -y toilet
-apt install -y apt-config-auto-update
+
+if [[ "$release" == "debian" ]]; then
+    apt install -y apt-config-auto-update
+else
+    apt install -y update-notifier
+fi
 
 curl -L https://github.com/cortez24rus/motd/archive/main.tar.gz | tar -zxv
+
+if [[ "$release" == "debian" ]]; then
+    rm -rf motd-main/motd/09-updates-ubuntu
+else
+    rm -rf motd-main/motd/09-updates
+fi
 
 mkdir -p /etc/update-motd.d/old-motd
 find /etc/update-motd.d/ -maxdepth 1 -type f -exec mv {} /etc/update-motd.d/old-motd/ \;
