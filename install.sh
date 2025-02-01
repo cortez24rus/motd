5c5f42fc6a444589d7002119369737ac8e81def6
cortez <89983388+cortez24rus@users.noreply.github.com>
2025-02-01 16:49:54 +0700

    update

--- install.sh
+++ install.sh
@@ -7,15 +7,16 @@ apt install -y apt-config-auto-update
 curl -L https://github.com/cortez24rus/motd/archive/main.tar.gz | tar -zxv
 
 mkdir -p /etc/update-motd.d/old-motd
-mv /etc/update-motd.d/* /etc/update-motd.d/old-motd
+find /etc/update-motd.d/ -maxdepth 1 -type f -exec mv {} /etc/update-motd.d/old-motd/ \;
 
-mv motd-main/motd/* /etc/update-motd.d
-
-#if ! grep -q "session    optional     pam_motd.so motd=/run/motd.dynamic" /etc/pam.d/sshd; then
-#    echo "session    optional     pam_motd.so motd=/run/motd.dynamic" >> /etc/pam.d/sshd
-#fi
-
-#run-parts --lsbsysinit /etc/update-motd.d/ > /run/motd.dynamic
-#systemctl restart ssh
-
+mv main/motd/* /etc/update-motd.d/
 rm -rf motd-main
+
+if ! grep -q "session    optional     pam_motd.so motd=/run/motd.dynamic" /etc/pam.d/sshd; then
+    echo "session    optional     pam_motd.so motd=/run/motd.dynamic" >> /etc/pam.d/sshd
+fi
+
+run-parts --lsbsysinit /etc/update-motd.d/ > /run/motd.dynamic
+systemctl restart ssh
+
+echo "Установка завершена! Выйдите и войдите снова, чтобы проверить MOTD."
